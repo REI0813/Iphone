0a4cf14cfc63a61481c38edde275b86f5a863a28
REI0813 <shrei.813315@gmail.com>
2026-08-07 16:04:38 +0900

    Iphone

--- src/com/github/rei0813/iphone/Iphone.java
+++ src/com/github/rei0813/iphone/Iphone.java
@@ -2,4 +2,15 @@ package com.github.rei0813.iphone;
 
 public class Iphone {
 
+	public static void main(String[] args){
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+		iphone.call();
+		iphone.mail();
+		iphone.photo();
+		iphone.internet();
+	}
 }
